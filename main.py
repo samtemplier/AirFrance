7f1e1177d0037017e83bada79cc1f7d7981efa83
AntoineCoulon02 <antcoul0902@gmail.com>
2023-11-09 19:16:37 +0100

    main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+from datetime import datetime, timedelta
 
-CheminTypeBain = r""
-CheminCycle = r""
+CheminTypeBain = r"./Données/Bains.json"
+CheminCycle = r"./Données/Cycles.json"
 
 # Ouvrir le fichier JSON en lecture
 with open(CheminTypeBain, 'r') as fichier:
@@ -13,9 +14,26 @@ with open(CheminCycle, 'r') as fichier:
     # Charger les données JSON
     TypeCycle = json.load(fichier)
 
-ListTest = ["HDL2524", "HDL-202 "]
-DicoTest = {}
-for i in range(len(ListTest)):
-    DicoTest[ListTest[i]]=TypeCycle[ListTest[i]]
+ListTest = ["Cycle_220", "Cycle_60"]
 
-#"frf"
+for cycleName in range(len(ListTest)):
+    cycle = TypeCycle[ListTest[cycleName]] #définition du cycle
+    bainList = cycle["Bain"] #selection des bains
+    dureeBain = cycle["Duree"] #selection des durées
+    bainUse = TypeBain[bainList[0]] #selection du bain
+    timeBain = datetime.strptime(bainUse["Time"], "%H:%M:%S").time()
+    stringBain = "00:" + str(dureeBain[0]) + ":00"
+    timeCycle = datetime.strptime(stringBain, "%H:%M:%S").time()
+
+    delta1 = timedelta(hours=timeBain.hour, minutes=timeBain.minute)
+    delta2 = timedelta(hours=timeCycle.hour, minutes=timeCycle.minute)
+
+    timeBain = delta1 + delta2
+    timeInit = datetime.strptime(cycle["Time"], "%H:%M:%S").time()
+    delta1 = timedelta(hours=timeInit.hour, minutes=timeInit.minute)
+    cycle["Time"] = str(delta1 + delta2)
+
+print(TypeCycle)
+
+
+    
